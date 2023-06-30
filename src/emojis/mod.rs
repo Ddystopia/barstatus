6c33e048d5fa12b2d17db843daac69558412f925
Oleksandr Babak <alexanderbabak@proton.me>
2023-06-30 16:33:46 +0200

    Refactor

--- src/emojis/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod animated_emoji;
-pub mod animated_emoji_builder;
-
-pub use animated_emoji::AnimatedEmoji;
-pub use animated_emoji_builder::AnimatedEmojiBuilder;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,31 @@
 use std::time::{Duration, SystemTime, SystemTimeError};
-pub mod emojis;
-pub mod metrics;
+pub mod emojis {
+    pub mod animated_emoji;
+    pub mod animated_emoji_builder;
+
+    pub use animated_emoji::AnimatedEmoji;
+    pub use animated_emoji_builder::AnimatedEmojiBuilder;
+}
+
+pub mod metrics {
+    pub mod battery;
+    pub mod bluetooth;
+    pub mod cpu;
+    pub mod date;
+    pub mod mem;
+    pub mod net;
+    pub mod update;
+    pub mod xkblayout;
+
+    pub use battery::BatteryMetric;
+    pub use bluetooth::BluetoothChargeMetric;
+    pub use cpu::CpuMetric;
+    pub use date::DateMetric;
+    pub use mem::MemMetric;
+    pub use net::NetMetric;
+    pub use update::UpdatesMetric;
+    pub use xkblayout::XkbLayoutMetric;
+}
 
 pub trait Metric {
     fn update(&mut self) {}
