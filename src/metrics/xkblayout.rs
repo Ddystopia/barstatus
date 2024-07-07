144d09218b83c56cac4215db04c9cfa6db51cd97
Oleksandr Babak <alexanderbabak@proton.me>
2024-07-07 21:21:56 +0200

    fix: xkg is too slow, time it

--- src/metrics/xkblayout.rs
+++ src/metrics/xkblayout.rs
@@ -8,21 +8,35 @@ use std::time::{Duration, Instant};
 #[derive(Debug)]
 pub struct XkbLayoutMetric {
     timeout: Duration,
+    last_updated_at: Cell<Instant>,
+    locale: RefCell<String>,
 }
 
 impl XkbLayoutMetric {
     #[must_use]
     pub fn new(timeout: Duration) -> Self {
-        Self { timeout }
+        Self {
+            timeout,
+            last_updated_at: Cell::new(Instant::now()),
+            locale: RefCell::new(String::new()),
+        }
     }
-}
 
     fn update(&self) -> Result<(), std::fmt::Error> {
         let out = Command::new("sh")
             .arg("-c")
             .arg("xkb-switch")
             .output()
-            .ok()?;
+            .map_err(|_| std::fmt::Error)?;
+
+        self.last_updated_at.set(Instant::now());
+
+        let loc = std::str::from_utf8(&out.stdout).map_err(|_| std::fmt::Error)?;
+        self.locale
+            .replace(loc.strip_suffix('\n').unwrap_or(loc).to_string());
+        Ok(())
+    }
+}
 
 impl Metric for XkbLayoutMetric {
     fn timeout(&self) -> Duration {
